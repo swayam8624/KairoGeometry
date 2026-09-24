78a5a7c524ce6412ce2933711e9680a7e31ebd01
SWAYAM SINGAL <swayam.singal@gmail.com>
2026-09-24 15:12:58 +0530

    Tests: keep geometry smoke checks active in Release

--- tests/smoke.cpp
+++ tests/smoke.cpp
@@ -1,7 +1,7 @@
 import Kairo.Foundation.Geometry;
 import Kairo.Foundation.Math;
 
-#include <cassert>
+#define KAIRO_REQUIRE(expression) do { if (!(expression)) return __LINE__; } while (false)
 
 int main()
 {
@@ -13,33 +13,33 @@ int main()
             Vec3f::Zero(),
             Vec3f::UnitX());
 
-    assert(NearlyEqual(ray.GetPoint(3.0f), Vec3f(3.0f, 0.0f, 0.0f)));
-    assert(NearlyEqual(ClosestPoint(ray, Vec3f(2.0f, 5.0f, 0.0f)), Vec3f(2.0f, 0.0f, 0.0f)));
+    KAIRO_REQUIRE(NearlyEqual(ray.GetPoint(3.0f), Vec3f(3.0f, 0.0f, 0.0f)));
+    KAIRO_REQUIRE(NearlyEqual(ClosestPoint(ray, Vec3f(2.0f, 5.0f, 0.0f)), Vec3f(2.0f, 0.0f, 0.0f)));
 
     const Planef ground =
         Planef::XZ();
 
-    assert(NearlyEqual(ground.SignedDistanceToPoint(Vec3f(0.0f, 2.0f, 0.0f)), 2.0f));
-    assert(NearlyEqual(ground.ProjectPoint(Vec3f(1.0f, 2.0f, 3.0f)), Vec3f(1.0f, 0.0f, 3.0f)));
+    KAIRO_REQUIRE(NearlyEqual(ground.SignedDistanceToPoint(Vec3f(0.0f, 2.0f, 0.0f)), 2.0f));
+    KAIRO_REQUIRE(NearlyEqual(ground.ProjectPoint(Vec3f(1.0f, 2.0f, 3.0f)), Vec3f(1.0f, 0.0f, 3.0f)));
 
     const Spheref unitSphere =
         Spheref::Unit();
 
-    assert(unitSphere.ContainsPoint(Vec3f(0.25f, 0.25f, 0.25f)));
-    assert(Intersects(unitSphere, Spheref::FromCenterRadius(Vec3f(1.5f, 0.0f, 0.0f), 0.75f)));
+    KAIRO_REQUIRE(unitSphere.ContainsPoint(Vec3f(0.25f, 0.25f, 0.25f)));
+    KAIRO_REQUIRE(Intersects(unitSphere, Spheref::FromCenterRadius(Vec3f(1.5f, 0.0f, 0.0f), 0.75f)));
 
     const Trianglef triangle =
         Trianglef::UnitRightTriangleXY();
 
-    assert(NearlyEqual(triangle.Area(), 0.5f));
-    assert(triangle.ContainsPoint(Vec3f(0.25f, 0.25f, 0.0f)));
-    assert(!triangle.ContainsPoint(Vec3f(1.25f, 0.25f, 0.0f)));
+    KAIRO_REQUIRE(NearlyEqual(triangle.Area(), 0.5f));
+    KAIRO_REQUIRE(triangle.ContainsPoint(Vec3f(0.25f, 0.25f, 0.0f)));
+    KAIRO_REQUIRE(!triangle.ContainsPoint(Vec3f(1.25f, 0.25f, 0.0f)));
 
     const AABBf unitBox =
         AABBf::Unit();
 
-    assert(unitBox.ContainsPoint(Vec3f(0.25f, 0.25f, 0.25f)));
-    assert(NearlyEqual(unitBox.ClosestPoint(Vec3f(2.0f, 0.0f, 0.0f)), Vec3f(0.5f, 0.0f, 0.0f)));
+    KAIRO_REQUIRE(unitBox.ContainsPoint(Vec3f(0.25f, 0.25f, 0.25f)));
+    KAIRO_REQUIRE(NearlyEqual(unitBox.ClosestPoint(Vec3f(2.0f, 0.0f, 0.0f)), Vec3f(0.5f, 0.0f, 0.0f)));
 
     const Capsulef capsule =
         Capsulef::FromEndpointsRadius(
@@ -47,16 +47,16 @@ int main()
             Vec3f(0.0f, 1.0f, 0.0f),
             0.5f);
 
-    assert(capsule.ContainsPoint(Vec3f(0.25f, 0.0f, 0.0f)));
-    assert(NearlyEqual(DistanceToCapsule(Vec3f(1.5f, 0.0f, 0.0f), capsule), 1.0f));
-    assert(Intersects(Spheref::FromCenterRadius(Vec3f(0.75f, 0.0f, 0.0f), 0.25f), capsule));
+    KAIRO_REQUIRE(capsule.ContainsPoint(Vec3f(0.25f, 0.0f, 0.0f)));
+    KAIRO_REQUIRE(NearlyEqual(DistanceToCapsule(Vec3f(1.5f, 0.0f, 0.0f), capsule), 1.0f));
+    KAIRO_REQUIRE(Intersects(Spheref::FromCenterRadius(Vec3f(0.75f, 0.0f, 0.0f), 0.25f), capsule));
 
     const OBBf obb =
         OBBf::FromAABB(unitBox);
 
-    assert(obb.ContainsPoint(Vec3f(0.25f, 0.25f, 0.25f)));
-    assert(NearlyEqual(ClosestPointOnOBB(obb, Vec3f(2.0f, 0.0f, 0.0f)), Vec3f(0.5f, 0.0f, 0.0f)));
-    assert(Intersects(unitBox, obb));
+    KAIRO_REQUIRE(obb.ContainsPoint(Vec3f(0.25f, 0.25f, 0.25f)));
+    KAIRO_REQUIRE(NearlyEqual(ClosestPointOnOBB(obb, Vec3f(2.0f, 0.0f, 0.0f)), Vec3f(0.5f, 0.0f, 0.0f)));
+    KAIRO_REQUIRE(Intersects(unitBox, obb));
 
     const Rayf obbRay =
         Rayf::FromOriginDirection(
@@ -66,9 +66,9 @@ int main()
     const auto obbHit =
         RayOBB(obbRay, obb);
 
-    assert(obbHit.has_value());
-    assert(NearlyEqual(obbHit->Distance, 1.5f));
-    assert(NearlyEqual(obbHit->Point, Vec3f(-0.5f, 0.0f, 0.0f)));
+    KAIRO_REQUIRE(obbHit.has_value());
+    KAIRO_REQUIRE(NearlyEqual(obbHit->Distance, 1.5f));
+    KAIRO_REQUIRE(NearlyEqual(obbHit->Point, Vec3f(-0.5f, 0.0f, 0.0f)));
 
     const Segmentf segmentA =
         Segmentf::FromEndpoints(
@@ -83,15 +83,15 @@ int main()
     const SegmentClosestResultf segmentResult =
         ClosestPoints(segmentA, segmentB);
 
-    assert(NearlyEqual(segmentResult.DistanceSquared, 1.0f));
-    assert(NearlyEqual(DistanceBetweenSegments(segmentA, segmentB), 1.0f));
+    KAIRO_REQUIRE(NearlyEqual(segmentResult.DistanceSquared, 1.0f));
+    KAIRO_REQUIRE(NearlyEqual(DistanceBetweenSegments(segmentA, segmentB), 1.0f));
 
     const Linef line =
         Linef::FromPointDirection(
             Vec3f::Zero(),
             Vec3f::UnitY());
 
-    assert(NearlyEqual(ClosestPointOnLine(line, Vec3f(2.0f, 3.0f, 0.0f)), Vec3f(0.0f, 3.0f, 0.0f)));
+    KAIRO_REQUIRE(NearlyEqual(ClosestPointOnLine(line, Vec3f(2.0f, 3.0f, 0.0f)), Vec3f(0.0f, 3.0f, 0.0f)));
 
     const Capsulef nearbyCapsule =
         Capsulef::FromEndpointsRadius(
@@ -99,17 +99,17 @@ int main()
             Vec3f(0.75f, 1.0f, 0.0f),
             0.5f);
 
-    assert(CapsuleCapsule(capsule, nearbyCapsule));
+    KAIRO_REQUIRE(CapsuleCapsule(capsule, nearbyCapsule));
 
     const Frustumf clipFrustum =
         Frustumf::FromViewProjection(
             Mat4f::Identity());
 
-    assert(clipFrustum.ContainsPoint(Vec3f(0.0f, 0.0f, 0.5f)));
-    assert(!clipFrustum.ContainsPoint(Vec3f(2.0f, 0.0f, 0.5f)));
-    assert(clipFrustum.IntersectsSphere(Spheref::FromCenterRadius(Vec3f::Zero(), 0.25f)));
-    assert(clipFrustum.IntersectsAABB(unitBox));
-    assert(clipFrustum.IntersectsOBB(obb));
+    KAIRO_REQUIRE(clipFrustum.ContainsPoint(Vec3f(0.0f, 0.0f, 0.5f)));
+    KAIRO_REQUIRE(!clipFrustum.ContainsPoint(Vec3f(2.0f, 0.0f, 0.5f)));
+    KAIRO_REQUIRE(clipFrustum.IntersectsSphere(Spheref::FromCenterRadius(Vec3f::Zero(), 0.25f)));
+    KAIRO_REQUIRE(clipFrustum.IntersectsAABB(unitBox));
+    KAIRO_REQUIRE(clipFrustum.IntersectsOBB(obb));
 
     return 0;
 }
